0996c4013748b53fc6b258b923f93e832c84685d
Adi Yaari <ayaari@aligntech.com>
2022-04-18 22:18:59 +0300

    logo navigate to home

--- src/components/LogoMenu/index.jsx
+++ src/components/LogoMenu/index.jsx
@@ -1,5 +1,18 @@
+import { useNavigate } from "react-router-dom";
 import Logo from "../../images/logo.png";
 
-export default () => <div>
-    <img src={Logo} width="250" height="80"/>
-</div>
+export default () => {
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <img
+        onClick={() => navigate("/")}
+        src={Logo}
+        width="250"
+        height="80"
+        style={{ cursor: "pointer" }}
+      />
+    </div>
+  );
+};
