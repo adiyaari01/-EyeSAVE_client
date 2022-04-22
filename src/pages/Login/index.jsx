86bfa9c2b699937de090c8078428783405cf4320
Adi Yaari <ayaari@aligntech.com>
2022-04-22 16:04:18 +0300

    login

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,12 +1,28 @@
-import {useSetRecoilState} from "recoil";
-import {IsLoggedInState} from "../../state/atoms";
-import { useNavigate } from "react-router-dom"
-import { Grid, Paper, Avatar, TextField, Checkbox, FormControlLabel, Button, Link, Typography } from "@mui/material";
+import { useSetRecoilState } from "recoil";
+import React, { useState } from "react";
+import { IsLoggedInState } from "../../state/atoms";
+import { useNavigate } from "react-router-dom";
+import {
+  Grid,
+  Paper,
+  Avatar,
+  TextField,
+  Checkbox,
+  FormControlLabel,
+  Button,
+  Link,
+  Typography,
+} from "@mui/material";
 import LockIcon from "@mui/icons-material/LockOutlined";
+import { login } from "../../api";
+import axios from "axios";
 
 const Login = () => {
   const navigate = useNavigate();
-  const setLoggedIn = useSetRecoilState(IsLoggedInState)
+  const setLoggedIn = useSetRecoilState(IsLoggedInState);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   const paperStyle = {
     padding: 20,
     width: 280,
@@ -17,17 +33,30 @@ const Login = () => {
     backgroundColor: "#BC88C9",
   };
   const btnStyle = {
-      margin: '8px 0',
+    margin: "8px 0",
   };
   const fieldStyle = {
-    margin: '8px 0'
-};
+    margin: "8px 0",
+  };
 
-  const handleLogin = () => {
-    // TODO: auth with server side
-    setLoggedIn(true);
-    navigate("/")
-  }
+  const handleLogin = async () => {
+    login(email, password);
+  };
+  // TODO: auth with server side
+  // setLoggedIn(true);
+  // navigate("/")
+  // const flag = await login(email, password);
+  // console.log(flag);
+  //     await axios({
+  //       method: "POST",
+  //       url: "http://localhost:8000/auth",
+  //       data: {
+  //         email: email,
+  //         password: password,
+  //       },
+  //     });
+  // };
+
   return (
     <div>
       <Grid align="center">
@@ -36,29 +65,47 @@ const Login = () => {
             <LockIcon />
           </Avatar>
           <h2>Sign In</h2>
-          <TextField style={fieldStyle}
+          <TextField
+            style={fieldStyle}
             label="Email"
             placeholder="Enter an email address"
             fullWidth
             required
+            onChange={() => {
+              setEmail(event.target.value);
+            }}
           ></TextField>
-          <TextField style={fieldStyle}
+          <TextField
+            style={fieldStyle}
             label="Password"
             placeholder="Enter a password"
             type="password"
             fullWidth
             required
+            onChange={() => {
+              setPassword(event.target.value);
+            }}
           ></TextField>
           <FormControlLabel
             control={<Checkbox defaultChecked color="default" />}
             label="remember me"
           />
-          <Button variant="contained" style={btnStyle} color="secondary" fullWidth onClick={handleLogin}>
+          <Button
+            variant="contained"
+            style={btnStyle}
+            color="secondary"
+            fullWidth
+            onClick={handleLogin}
+          >
             SIGN IN
           </Button>
-          <Typography><Link href="#">Forgot password?</Link></Typography>
+          <Typography>
+            <Link href="#">Forgot password?</Link>
+          </Typography>
           {/* TODO: change syntax*/}
-          <Typography >Do you have an account?<Link href="#">Sign up</Link></Typography>
+          <Typography>
+            Do you have an account?<Link href="#">Sign up</Link>
+          </Typography>
         </Paper>
       </Grid>
     </div>
