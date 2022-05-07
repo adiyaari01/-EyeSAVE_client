14d079bfba7a8a41ebac28c2e5a6001440d4e808
Adi Yaari <ayaari@aligntech.com>
2022-05-07 17:43:34 +0300

    IsLoggedInState

--- src/state/atoms.jsx
+++ src/state/atoms.jsx
@@ -27,7 +27,7 @@ const initializeEventsState = ({setSelf, onSet}) => {
 
 export const IsLoggedInState = atom({
     key: "IsLoggedInState",
-    default: true //TODO: change to false
+    default: false //TODO: change to false
 })
 
 export const childrenAttendanceState = atom({
