981c4c3e90c06666a65de4d6fc4d6cf793d44ff0
Adi Yaari <ayaari@aligntech.com>
2022-05-07 14:49:47 +0300

    css

--- src/pages/Home/GridUseStyles.jsx
+++ src/pages/Home/GridUseStyles.jsx
@@ -5,6 +5,7 @@ export const CardUseStyles = makeStyles({
         backgroundColor: "#3F424C !important",
         color: "#E3E3E3 !important",
         borderRadius : "16px !important",
-        cursor: "pointer"
+        cursor: "pointer",
+        maxHeight:"200px"
     }
 })
