e90445d10a3739e41d9903e46e58e127eb0b7eb9
Adi Yaari <ayaari@aligntech.com>
2022-05-09 12:19:11 +0300

    css

--- src/pages/WatchLive/index.jsx
+++ src/pages/WatchLive/index.jsx
@@ -7,12 +7,14 @@ export default () => {
       direction={"row"}
       flexWrap="wrap"
       gap={3}
+      justifyContent="center"
+
       >
         {/* <video ref={videoRef}></video> */}
-        <iframe width="400" height="230" src="https://rtsp.me/embed/NHANEa74/" frameBorder="0"></iframe>     
-        <iframe width="400" height="230" src="https://rtsp.me/embed/NHANEa74/" frameBorder="0"></iframe>     
-        <iframe width="400" height="230" src="https://rtsp.me/embed/NHANEa74/" frameBorder="0"></iframe>     
-        <iframe width="400" height="230" src="https://rtsp.me/embed/NHANEa74/" frameBorder="0"></iframe>     
+        <iframe width="440" height="270" src="https://rtsp.me/embed/NHANEa74/" frameBorder="0"></iframe>     
+        <iframe width="440" height="270" src="https://rtsp.me/embed/NHANEa74/" frameBorder="0"></iframe>     
+        <iframe width="440" height="270" src="https://rtsp.me/embed/NHANEa74/" frameBorder="0"></iframe>     
+        <iframe width="440" height="270" src="https://rtsp.me/embed/NHANEa74/" frameBorder="0"></iframe>     
         {/* <iframe width="420" height="345" src="https://www.youtube.com/embed/tgbNymZ7vqY"></iframe>
         <iframe width="400" height="240" src="https://www.youtube.com/embed/6JK1Pp0j0nQ" frameBorder="0" allowFullScreen></iframe>
         <img crossorigin="anonymous" name="main" id="main" border="0" width="640" height="480" src="https://rtsp.me/embed/RhHSG8bA/"/>
